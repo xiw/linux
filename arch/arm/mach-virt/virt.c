93a47ff02b41242bd02f03c226e0b58f69a85061
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-04-28 14:40:51 +0000

    arm: introduce psci_smp_ops
    Rename virt_smp_ops to psci_smp_ops and move them to arch/arm/kernel/psci_smp.c.
    Remove mach-virt/platsmp.c, now unused.
    Compile psci_smp if CONFIG_ARM_PSCI and CONFIG_SMP.
    
    Add a cpu_die smp_op based on psci_ops.cpu_off.
    
    Initialize PSCI before setting smp_ops in setup_arch.
    
    If PSCI is available on the platform, prefer psci_smp_ops over the
    platform smp_ops.
    
    Changes in v9:
    - move comment about PSCI interface from psci.h to psci_smp.c.
    
    Changes in v8:
    - merge "prefer psci_smp_ops over mdesc->smp" into this patch.
    
    Changes in v6:
    - fixed return values for psci_smp_available and psci_init ifndef
    CONFIG_ARM_PSCI.
    
    Changes in v5:
    - document psci_operations;
    - psci_init returns NULL.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    CC: will.deacon@arm.com
    CC: arnd@arndb.de
    CC: marc.zyngier@arm.com
    CC: linux@arm.linux.org.uk
    CC: nico@linaro.org
    CC: rob.herring@calxeda.com

--- arch/arm/mach-virt/virt.c
+++ arch/arm/mach-virt/virt.c
@@ -43,12 +43,9 @@ static const char *virt_dt_match[] = {
 	NULL
 };
 
-extern struct smp_operations virt_smp_ops;
-
 DT_MACHINE_START(VIRT, "Dummy Virtual Machine")
 	.init_irq	= irqchip_init,
 	.init_time	= virt_timer_init,
 	.init_machine	= virt_init,
-	.smp		= smp_ops(virt_smp_ops),
 	.dt_compat	= virt_dt_match,
 MACHINE_END
